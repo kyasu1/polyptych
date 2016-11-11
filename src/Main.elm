ac755575911a453bb723bffc3933fffb5f8b78ec
kyasu1 <kyasuyako@gmail.com>
2016-11-11 22:23:15 +0900

    Show two images

--- src/Main.elm
+++ src/Main.elm
@@ -17,12 +17,24 @@ type alias Size =
 
 type Frame
     = SingleImage { url : String }
+    | HorizontalSplit
+        { top : Frame
+        , topHeight : Int
+        , bottom : Frame
+        }
 
 
 initialModel : Model
 initialModel =
-    { canvas = { width = 250, height = 250 }
-    , frame = SingleImage { url = "http://item.shopping.c.yimg.jp/i/l/pawnshopiko_12201-0285-001" }
+    { canvas =
+        { width = 250, height = 250 }
+        --    , frame = SingleImage { url = "http://item.shopping.c.yimg.jp/i/l/pawnshopiko_12201-0285-001" }
+    , frame =
+        HorizontalSplit
+            { top = SingleImage { url = "http://item.shopping.c.yimg.jp/i/l/pawnshopiko_12201-0285-001" }
+            , topHeight = 80
+            , bottom = SingleImage { url = "http://item.shopping.c.yimg.jp/i/l/pawnshopiko_12101-1115-001" }
+            }
     }
 
 
@@ -55,10 +67,25 @@ viewFrame size frame =
                 [ style
                     [ ( "background-image", "url(" ++ url ++ ")" )
                     , ( "background-size", "auto " ++ toString size.height ++ "px" )
-                    , ( "height", "100%" )
+                    , ( "width", toString size.width ++ "px" )
+                    , ( "height", toString size.height ++ "px" )
                     ]
                 ]
                 []
+
+        HorizontalSplit { top, topHeight, bottom } ->
+            div []
+                [ viewFrame
+                    { width = size.width
+                    , height = topHeight
+                    }
+                    top
+                , viewFrame
+                    { width = size.width
+                    , height = size.height - topHeight
+                    }
+                    bottom
+                ]
 
 
 view : Model -> Html.Html Msg
